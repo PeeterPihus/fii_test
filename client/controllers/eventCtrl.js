50642341bb32181489dd643d3582a599b2f9b03c
Peeter <peeter.pihus@khk.ee>
2018-01-15 16:30:08 +0200

    test

--- client/controllers/eventCtrl.js
+++ client/controllers/eventCtrl.js
@@ -1,13 +1,31 @@
 var eventCtrl = angular.module('eventCtrl', []);
 
 eventCtrl.controller('eventCtrl', function($scope, eventService){
-    eventService.getNextEvents().then(function(data){
-        console.log(data[0]);
-        $scope.event = data[0];
-        if(data.length === 0){
-            $scope.eventDialog = true;
-        }else{
-            $scope.eventDialog = false;
+    eventService.getEvents().then(function(data){
+        console.log(data);
+        $scope.events = data;
+        // if(data.length === 0){
+        //     $scope.eventDialog = true;
+        //     $scope.eventHeadline = false;
+        // }else{
+        //     $scope.eventDialog = false;
+        //     $scope.eventHeadline = true;
+        // }
+        angular.forEach($scope.events, function(value) {
+            value.date = new Date(value.date);
+        })
+
+        $scope.filterDate = function(events) {
+            var today = new Date();
+            var today_plus_one_day = new Date().setDate(today.getDate()-1);
+            return events.date >= today_plus_one_day;
+            if (event.date.length === 0){
+                $scope.eventDialog = false;
+                $scope.eventHeadline = true;
+            }else{
+                $scope.eventDialog = true;
+                $scope.eventHeadline = false;
+            }
         }
     }, function(err){
         console.log(err);
@@ -16,4 +34,5 @@ eventCtrl.controller('eventCtrl', function($scope, eventService){
     $scope.openModal = function(path){
         $scope.activeEventImg = path;
     }
+
 })
